9d01ae1f472881e5431db49d8a2ff165c4b13db0
witoo-source <133872532+witoo-source@users.noreply.github.com>
2025-08-20 18:41:51 +0200

    Added storefront endpoint

--- index.ts
+++ index.ts
@@ -9,7 +9,28 @@ app.use(cors())
 type AuthyRequest = Request<{}, {}, {
     accessToken: string
     entitlementsToken?: string
+    puuid: string
 }>
+
+app.post('/storefront', async (req: AuthyRequest, res: Response) => {
+    console.log(req.body)
+    await fetch(`https://pd.eu.a.pvp.net/store/v3/storefront/${req.body.puuid}`, {
+        method: "POST",
+        headers: {
+            "X-Riot-ClientPlatform":   "ew0KCSJwbGF0Zm9ybVR5cGUiOiAiUEMiLA0KCSJwbGF0Zm9ybU9TIjogIldpbmRvd3MiLA0KCSJwbGF0Zm9ybU9TVmVyc2lvbiI6ICIxMC4wLjE5MDQyLjEuMjU2LjY0Yml0IiwNCgkicGxhdGZvcm1DaGlwc2V0IjogIlVua25vd24iDQp9",
+            "X-Riot-ClientVersion": "B312378013F36E38",
+            "X-Riot-Entitlements-JWT": req.body.entitlementsToken,
+            "Authorization": `Bearer ${req.body.accessToken}`,
+        } as Record<string, string>,
+        body: JSON.stringify({})
+    })
+    .then(res => res.json())
+    .then(data => {
+        console.log(data)
+        res.json(data)
+    })
+    .catch(e => res.status(500).json({ error: e.message || e }));
+})
 
 app.post('/entitlements', async (req: AuthyRequest, res: Response) => {
     console.log(req.body)
